500ec8df16fd4f8e77fb2742446ac5c0027d3e44
Anton_Ramanau <anton61499@gmail.com>
2022-06-29 13:25:05 +0200

    Update view of the index page.

--- src/main/java/com/example/ftc/customer/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/ftc/customer/configuration/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/registration", "/", "/css/**", "/js/**").permitAll()
+                .antMatchers("/registration", "/", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
